06bdaff839279071f19ff8ca78bd0c5205c81b8a
jakejie <jakejie@163.com>
2020-07-05 20:28:05 +0800

    添加样例数据

--- start_alibaba.py
+++
@@ -1,8 +0,0 @@
-# coding:utf-8
-import os
-import sys
-from scrapy import cmdline
-
-if __name__ == '__main__':
-    sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-    cmdline.execute(['scrapy', 'crawl', 'alibaba'])
